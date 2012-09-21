558c4beb6aa22752113376ddd7de12aab075d1ec
Kaushik Sarkar <ksarkar1@asu.edu>
2012-09-21 14:26:02 -0700

    Small change - experimental.

--- edu.asu.kaushik.ai.project1/src/edu/asu/kaushik/ai/project1/valueiter/ValueIteration.java
+++ edu.asu.kaushik.ai.project1/src/edu/asu/kaushik/ai/project1/valueiter/ValueIteration.java
@@ -2,9 +2,9 @@ package edu.asu.kaushik.ai.project1.valueiter;
 
 public class ValueIteration {
 
-	/**
-	 * @param args
-	 */
+	public void valueIteratior() {
+		
+	}
 	public static void main(String[] args) {
 		System.out.println("This is the class for value iteration.");
 
