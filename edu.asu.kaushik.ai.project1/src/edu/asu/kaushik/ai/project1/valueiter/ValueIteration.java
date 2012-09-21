e7b674182c3b558b5cb9c007f10d6d167bbfefee
Kaushik Sarkar <ksarkar1@asu.edu>
2012-09-21 14:23:16 -0700

    First commit.

---
+++ edu.asu.kaushik.ai.project1/src/edu/asu/kaushik/ai/project1/valueiter/ValueIteration.java
@@ -0,0 +1,13 @@
+package edu.asu.kaushik.ai.project1.valueiter;
+
+public class ValueIteration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is the class for value iteration.");
+
+	}
+
+}
